c44af24869cb6d3ae99bb511f275ef6f3cf09a7a
KarthivGithub <valdaskarthikgoud@gmail.com>
2025-03-07 00:46:51 +0530

    removed comments

--- src/data.py
+++ src/data.py
@@ -1,11 +1,3 @@
-# import kagglehub
-
-# # Download latest version
-# path = kagglehub.dataset_download("elemento/nyc-yellow-taxi-trip-data")
-
-# print("Path to dataset files:", path)
-
-
 import os
 import boto3
 import kagglehub
